5888015637dd6cf4ef9b95af556b92e59321c864
chiyun <chiyundevelop@gmail.com>
2021-09-23 15:11:17 +0800

    add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo npm install -g hexo-cli 
+npm install hexo -g
+npm install hexo-deployer-git --save
+npm install -save hexo-helper-live2d
+git clone https://github.com/theme-next/hexo-theme-next themes/next
+
+#blog:
+#https://www.cxybb.com/article/as480133937/100138838
+
